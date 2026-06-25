313d422d716c4835508b151f7c6962b448a6b59d
asanoopsharm3 <asanoopsharma03@gmail.com>
2026-06-26 00:13:17 +0530

    change transaction id

--- src/mtn/mtn-transaction-id.util.ts
+++ src/mtn/mtn-transaction-id.util.ts
@@ -1,15 +1,25 @@
-import { randomBytes } from 'crypto';
+import { randomInt } from 'crypto';
 import { MTN_REQUEST_TRANSACTION_ID_LENGTH } from './mtn.constants';
 
-/** 8-char hex id e.g. a1b2c3c7 for MTN request header transactionid. */
+const MTN_TXN_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+/** 8-char alphanumeric id e.g. a1b2c3r4 for MTN request header transactionid. */
 export function generateMtnRequestTransactionId(): string {
-  return randomBytes(MTN_REQUEST_TRANSACTION_ID_LENGTH / 2).toString('hex');
+  let id = '';
+
+  for (let i = 0; i < MTN_REQUEST_TRANSACTION_ID_LENGTH; i++) {
+    id += MTN_TXN_CHARS[randomInt(0, MTN_TXN_CHARS.length)];
+  }
+
+  return id;
 }
 
 export function generateUniqueMtnRequestTransactionIds(count: number): string[] {
   const ids = new Set<string>();
+
   while (ids.size < count) {
     ids.add(generateMtnRequestTransactionId());
   }
+
   return [...ids];
 }
